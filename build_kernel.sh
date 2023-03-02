864875330246afbfaa729d34c124fb07fc27f506
physwizz <pollockg86@gmail.com>
2023-03-02 14:44:49 +1100

    build_kernel.sh updated for dtb

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
+
 make clean && make mrproper
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
+export DTB_LOC=$(pwd)/arch/arm64/boot/dts
+export TOOLS_LOC=$(pwd)/scripts/tools/bin
 make physwizz_defconfig
 make
+
+$TOOLS_LOC/mkdtboimg.py cfg_create $DTB_LOC/dtb.img --dtb-dir $DTB_LOC/exynos $TOOLS_LOC/dtb.cfg
 
